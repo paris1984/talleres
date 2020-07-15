030629375ee47e3d8b63875d9950259cef4b079c
darkparis <jlmartinr1984@gmail.com>
2020-07-15 16:31:52 +0200

    feature.2 conexion a base de datos sqlite

--- src/main/java/es/pc/controller/HealthController.java
+++ src/main/java/es/pc/controller/HealthController.java
@@ -1,16 +1,21 @@
 package es.pc.controller;
 
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HealthController {
+import java.sql.Connection;
 
+@RestController
+@Slf4j
+public class HealthController {
 
     @GetMapping("/health")
     public String healthCheck(){
-        System.out.println("Health check OK");
+        log.info("Health check OK");
         return "OK";
     }
 }
--- src/main/java/es/pc/main/Application.java
+++ src/main/java/es/pc/main/Application.java
@@ -1,5 +1,6 @@
 package es.pc.main;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,5 +12,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
