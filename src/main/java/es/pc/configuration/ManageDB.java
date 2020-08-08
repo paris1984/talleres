a1704638d9697300eebc6cd3348457de13e8d9b8
darkparis <jlmartinr1984@gmail.com>
2020-08-08 17:19:03 +0200

    Feature.4 Layer service,repository get car json

--- src/main/java/es/pc/configuration/ManageDB.java
+++ src/main/java/es/pc/configuration/ManageDB.java
@@ -6,24 +6,18 @@ import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
 @Configuration
 @Slf4j
 public class ManageDB {
 
-    private static final String CREATION_SQL = "create table IF NOT EXISTS vehiculos(\n" +
-            "matricula varchar(15) primary key,\n" +
-            "tipo      varchar(20)\n" +
-            ");";
     @Bean
     public Connection connection(){
         Connection c = null;
         try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            Statement stmt = c.createStatement();
-            stmt.execute(CREATION_SQL);
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String sURL = "jdbc:mysql://localhost:3306/vehiculos";
+            c = DriverManager.getConnection(sURL,"user","user");
         } catch ( Exception e ) {
             log.error("Error al crear la conexion:" +e.getMessage());
         }
